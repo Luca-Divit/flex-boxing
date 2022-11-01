60bc8aa00358a147b730f36d7f7f48c003c2a8ba
Luca Di Vittorio <luca.div@hotmail.com>
2022-11-01 11:45:31 +0000

    completed layout and effect, although to be refactored as it is not smooth enough

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,14 @@
-console.log('Hello from JS!')
+const panels = document.querySelectorAll('.panel');
+console.log(panels);
+
+// panels.addEventListener('click', e => {
+//   console.log(e);
+// })
+
+panels.forEach(
+  panel => panel.addEventListener(
+    'click', e => {
+      console.log(e.currentTarget.classList.toggle('open'))
+    }
+  )
+  )
